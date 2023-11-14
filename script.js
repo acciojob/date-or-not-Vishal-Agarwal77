c944fc1d188f66f53586838f7c50d4f53154a440
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-15 01:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var isDate = function (input) {
   //   write your code here
+	let d=new Date(input);
+	if(d=="Invalid Date"){
+		return false;
+	}
+	return true;
 };
 
-// Do not change the code below.
+// Do not change the code below.\
 const input = prompt("Enter Date.");
 alert(isDate(input));
